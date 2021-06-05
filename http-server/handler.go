6268c505ba34d4e8f09e11ff85b5d86c3908b50f
Owen P. F. Li <pfowenli@gmail.com>
2021-06-05 23:54:34 +0800

    reupload handler.go

--- http-server/handler.go
+++ http-server/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -21,34 +22,34 @@ func helloWorld(w http.ResponseWriter, req *http.Request) {
 func headers(w http.ResponseWriter, req *http.Request) {
 	for field, values := range req.Header {
 		for _, value := range values {
-			fmt.Printf("%v: %v\n", field, value)
+			log.Printf("%v: %v\n", field, value)
 			fmt.Fprintf(w, "%v: %v\n", field, value)
 		}
 	}
 }
 
 func pathParameters(w http.ResponseWriter, req *http.Request) {
-	fmt.Printf("path parameters\n")
+	log.Printf("path parameters")
 
 	for field, value := range mux.Vars(req) {
-		fmt.Printf("%v: %v\n", field, value)
+		log.Printf("%v: %v", field, value)
 		fmt.Fprintf(w, "%v: %v\n", field, value)
 	}
 }
 
 func queryStrings(w http.ResponseWriter, req *http.Request) {
-	fmt.Printf("query strings\n")
+	log.Printf("query strings")
 
 	for field, values := range req.URL.Query() {
 		for _, value := range values {
-			fmt.Printf("%v: %v\n", field, value)
+			log.Printf("%v: %v", field, value)
 			fmt.Fprintf(w, "%v: %v\n", field, value)
 		}
 	}
 }
 
 func jsonBody(w http.ResponseWriter, req *http.Request) {
-	fmt.Printf("json body\n")
+	log.Printf("json body")
 
 	body, err := ioutil.ReadAll(req.Body)
 	defer req.Body.Close()
@@ -68,8 +69,8 @@ func jsonBody(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	fmt.Printf("Id: %v\n", data.Id)
-	fmt.Printf("Type: %v\n", data.Type)
+	log.Printf("Id: %v", data.Id)
+	log.Printf("Type: %v", data.Type)
 
 	fmt.Fprintf(w, "Id: %v\n", data.Id)
 	fmt.Fprintf(w, "Type: %v\n", data.Type)
